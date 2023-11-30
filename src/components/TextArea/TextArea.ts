ef937394d2ff124c495b877df92eda4b2dc18e1e
Demirev Nikita <nikdem00@gmail.com>
2023-11-30 14:08:15 +0300

    запрогал немножечко один компонент, кажется я начал поонимать как работает эта магия
    на этот коммит, немного кода ушло, сори. Через час вернусь и мега-забоотаю

---
+++ src/components/TextArea/TextArea.ts
@@ -0,0 +1,19 @@
+import { Component } from "../../shared/services/snail/component";
+import { createElement, createComponent, createText } from "../../shared/services/vdom/VirtualDOM";
+
+export interface TextAreaProps {
+
+}
+
+export class TextArea extends Component<TextAreaProps, {}> {
+    render() {
+        const { ...textAreaProps } = this.props;
+
+        return createElement(
+            'textarea',
+            {
+                ...textAreaProps
+            },
+        )
+    }
+}
